fbce1c828169ffafa1b425ec4f70f418d4fe53a9
Luke Dol <sukh.dol77@gmail.com>
2020-06-21 22:30:02 -0600

    Added Make and Model tables.

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace vega.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using vega.Models;
+
+namespace vega.Persistence
 {
-    public class VegaDbContext
+    public class VegaDbContext : DbContext
     {
-        
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Make> Makes { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using vega.Persistence;
 
 namespace vega
 {
@@ -20,6 +22,10 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("Default"))
+                );
+            
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
