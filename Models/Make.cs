76cddb15709cd9c8bcd9362ec1258d930545051b
Luke Dol <sukh.dol77@gmail.com>
2020-06-21 23:21:22 -0600

    Added Make and Model Validations.

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
@@ -11,6 +12,9 @@ namespace vega.Models
         }
         
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public ICollection<Model> Models { get; set; }
