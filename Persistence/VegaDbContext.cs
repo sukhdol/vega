b0a3beb3aae8903c65d3069ac9b732f9162e81da
Luke Dol <sukh.dol77@gmail.com>
2020-06-22 14:39:50 -0600

    Added Model and Vehicle dbsets.

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -13,6 +13,8 @@ namespace vega.Persistence
 
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
