a7dc2a266cfda5ff90ad4eb2a051a13d7bfc846e
Luke Dol <sukh.dol77@gmail.com>
2020-06-21 21:20:58 -0600

    Added Make and Model relationship.

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace vega.Models
 {
     public class Make
     {
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+        
         public int Id { get; set; }
         public string Name { get; set; }
 
--- Models/Model.cs
+++ Models/Model.cs
@@ -5,5 +5,9 @@ namespace vega.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public int MakeId { get; set; }
+        
+        public Make Make { get; set; }
     }
 }
