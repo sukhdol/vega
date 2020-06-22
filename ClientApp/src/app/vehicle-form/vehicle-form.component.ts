e3f98e63ef3bc98dbb21ec78fdef5aa2ed18bf90
Luke Dol <sukh.dol77@gmail.com>
2020-06-22 10:20:10 -0600

    Applied vehicles service.

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {MakeService} from '../services/make.service';
-import {FeatureService} from '../services/feature.service';
+import {VehicleService} from '../services/vehicle.service';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -13,16 +12,15 @@ export class VehicleFormComponent implements OnInit {
   features: any[];
   vehicle: any = {};
 
-  constructor(private makeService: MakeService,
-              private featureService: FeatureService) {
+  constructor(private vehicleService: VehicleService) {
   }
 
   ngOnInit(): void {
-    this.makeService.getMakes().subscribe(makes => {
+    this.vehicleService.getMakes().subscribe(makes => {
       this.makes = makes;
     });
 
-    this.featureService.getFeatures().subscribe(features => {
+    this.vehicleService.getFeatures().subscribe(features => {
       this.features = features;
     });
   }
