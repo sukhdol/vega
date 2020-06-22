ee2ad9eef6b1928ed529e53ef07feb168f81a5c5
Luke Dol <sukh.dol77@gmail.com>
2020-06-22 14:41:10 -0600

    Added the Vehicle Create api.

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public VehiclesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var model = await _context.Models.FindAsync(vehicleResource.ModelId);
+
+            if (model == null)
+            {
+                ModelState.AddModelError("ModelId", "Invalid Model Id");
+                return BadRequest(ModelState);
+            }
+            
+            var vehicle = _mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+
+            _context.Vehicles.Add(vehicle);
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+            
+            return Ok(result);
+        }
+    }
+}
