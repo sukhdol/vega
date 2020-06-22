0446d94fb505e1e86573f9181a8e60a5ebdbcf8c
Luke Dol <sukh.dol77@gmail.com>
2020-06-22 01:40:46 -0600

    Added the vehicle form components.

---
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html'
+})
+export class VehicleFormComponent {
+
+}
