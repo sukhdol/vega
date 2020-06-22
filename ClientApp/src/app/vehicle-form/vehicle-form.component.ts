baedcfef7f84b0c56f9d454b6eb86f69ecb5eb49
Luke Dol <sukh.dol77@gmail.com>
2020-06-22 09:01:44 -0600

    Added make and model dropdown lists.

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MakeService} from "../services/make.service";
 
 @Component({
   selector: 'app-vehicle-form',
-  templateUrl: './vehicle-form.component.html'
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
 })
-export class VehicleFormComponent {
+export class VehicleFormComponent implements OnInit {
+  makes: any[];
+  vehicle: any = {};
+  models: any[];
+
+  constructor(private makeService: MakeService) {
+  }
+
+  ngOnInit(): void {
+    this.makeService.getMakes().subscribe(makes => {
+      this.makes = makes;});
+  }
+
+  onMakeChange() {
+    let selectedMake = this.makes.find(x => x.id == this.vehicle.make);
+
+    this.models = selectedMake ? selectedMake.models : [];
+  }
 
 }
